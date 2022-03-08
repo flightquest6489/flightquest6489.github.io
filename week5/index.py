896f986102d612041303c733876f1bac0fdc7fc1
Pujan Kavi <flightquest6489@users.noreply.github.com>
2022-03-07 20:33:00 -0500

    commit

---
+++ week5/index.py
@@ -0,0 +1,17 @@
+#Assignment part 1
+#create a list called myList with 4 values
+#mtList=[2,3,4,12]
+#assign values from position 1 to the end of myList to a new list myList2
+#append a new item to myList2 , remove the 3rd item then
+#assign the list to myList3
+
+
+mylist = [2,3,4,12]
+mylist2= []
+
+
+mylist[1:-1] = mylist2
+print(list1)
+mylist2.append(5)
+mylist2.pop(2)
+print(mylist2)
