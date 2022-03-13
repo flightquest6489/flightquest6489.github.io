957fc9150cf7318739db73655f067a0ee13deab7
Pujan Kavi <flightquest6489@users.noreply.github.com>
2022-03-13 19:40:43 -0400

    commit

--- week 2/html/Assignment 5/Script.js
+++ week 2/html/Assignment 5/Script.js
@@ -1,8 +1,7 @@
-function conversionTable(tagId, from, to){
+function conversionTable(tagId, from){
     var first = document.getElementById("Input1")
-    var second = document.getElementById("Input2")
+  
     from=first;
-    to=second;
     var conv = document.getElementById(tagId);
     var tab = document.getElementById("table");
     var bod = document.createElement("tbody");
@@ -19,17 +18,11 @@ function conversionTable(tagId, from, to){
     th.appendChild(document.createTextNode("Fahrenheit"));
     conv.appendChild(tab);
 }
-for (var i = from; i<=to; i++){
-    tr.document.createElement("tr");
-    bod.appendChild(tr);
-    td = document.createElement("td");
-    tr.appendChild(td);
-    td.appendChild(document.createTextNode(i));
+
     td=document.createElement("td");
     tr.appendChild(td);
     td.appendChild(document.createTextNode(celsiustofahrenheit(i)));
-}
+
 function celsiustofahrenheit(c) { return Math.round((c * 1.8)+32)}
 
 from = parseInt(first.value);
-to = parseInt(second.value);
